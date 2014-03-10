3da170f43d07a0d3e669a413cdafbc0719b21420
Josh <joshlemer@gmail.com>
2014-03-10 18:13:51 -0500

    Pull Algorithm WOrks

--- rumor.py
+++ rumor.py
@@ -1,4 +1,5 @@
 import random
+from sets import Set
 
 class Node:
 	def __init__(self, net, index):
@@ -12,7 +13,6 @@ class Node:
 		self.knows_rumor = True
 		
 		if self.state is 'A': self.state = 'B'
-
 	
 class Network:
 	def __init__(self, size):
@@ -34,11 +34,44 @@ class Network:
 			print ("(%d %d)" % (Node.index, Node.knows_rumor)),
 		print "\n"
 
+class Pull_alg:
+	def __init__(self, size):
+		self.the_net = Network(size)
+		self.the_net.rand_src()
+		self.turns_elapsed = 0
+		self.connections_made = 0
+		self.transmissions_made = 0
+	
+	def do_turn(self, turns):
+
+		for _ in range(0, turns):
+			nodes_to_tell = []
+			
+			for Node in self.the_net.Nodes:
+				if not Node.knows_rumor:
+					rand = random.choice(Node.net.Nodes)
+					while rand.index == Node.index:
+						rand = random.choice(Node.net.Nodes)
+
+					self.connections_made += 1
+
+					if rand.knows_rumor:
+						nodes_to_tell.append(Node)
+						self.transmissions_made += 1
+						print "%d told %d" % (rand.index, Node.index)
+
+			for Node in nodes_to_tell:
+				Node.tell_rumor()
+
+			self.turns_elapsed += 1
+
 class  Push_alg:
 	def __init__(self, size):
 		self.the_net = Network(size)
 		self.the_net.rand_src()
 		self.turns_elapsed = 0
+		self.connections_made = 0
+		self.transmissions_made = 0
 
 	def do_turn(self, turns):
 
@@ -48,13 +81,20 @@ class  Push_alg:
 			for Node in self.the_net.Nodes:
 				if Node.knows_rumor:
 					rand = random.choice(Node.net.Nodes)
+					while rand.index == Node.index:
+						rand = random.choice(Node.net.Nodes)
+
+					self.connections_made += 1
+
 					nodes_to_tell.append(rand)
+					self.transmissions_made += 1
 
 			for Node in nodes_to_tell:
 				Node.tell_rumor()
 
 			self.turns_elapsed += 1
 			
+print "Pushing"
 b = Push_alg(10)
 b.the_net.print_net()
 
@@ -62,12 +102,14 @@ for _ in range(0,10):
 	b.do_turn(1)
 	b.the_net.print_net()
 
+print "connections: %d transmissions: %d" % (b.connections_made, b.transmissions_made)
 
+print "Pulling"
+b = Pull_alg(10)
+b.the_net.print_net()
 
-					
+for _ in range(0,10):
+	b.do_turn(1)
+	b.the_net.print_net()
 
-		
-
-
-
-
+print "connections: %d transmissions: %d" % (b.connections_made, b.transmissions_made)
